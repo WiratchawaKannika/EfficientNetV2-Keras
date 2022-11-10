f965a37e00741cbe19d226ea14ea941b3f25c257
WiratchawaKannika <kannikaw@kkumail.com>
2022-11-10 15:28:01 +0700

    14classes-resume usai

--- EfficientNetV2_USAI_Viewing_Angle_fold1_2_R1_UnfreezeFullyConne-14classes-resume.py
+++ EfficientNetV2_USAI_Viewing_Angle_fold1_2_R1_UnfreezeFullyConne-14classes-resume.py
@@ -25,7 +25,7 @@ tf_device='/gpu:1'
 #Setting
 BATCH_SIZE = 16
 TARGET_SIZE = (480, 480)  # M variant expects images in shape (480, 480)
-epochs = 67  ##***--- Core Dump at Epoch 133 
+epochs = 67  ##***--- Core Dump at Epoch 133
 
 #load model
 from tensorflow.keras.models import load_model
